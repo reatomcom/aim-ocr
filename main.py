69c1508d6b0c7680a3154c359981f33ce0422ba3
re-adrianskrievins <152814934+re-adrianskrievins@users.noreply.github.com>
2023-12-06 21:08:30 +0200

    output formatting

--- main.py
+++ main.py
@@ -8,15 +8,19 @@ def run_pytesseract(image_path):
 
 
 def process_dataset(ocr_function):
-    root = config.DATASET_DIR
-    images = [x for x in root.iterdir() if x.is_file()]
-
     config.OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
     output_file = config.OUTPUT_DIR.joinpath(ocr_function.__name__).with_suffix(".txt")
 
     with output_file.open("w", encoding="utf-8") as f:
-        for image in images:
-            f.write(f"{image}\n{ocr_function(root.joinpath(image))}\n\n")
+        for image_path in config.DATASET_DIR.iterdir():
+            if not image_path.is_file():
+                continue
+
+            file_name = image_path.name
+            title = "{0}\n{1}\n{0}".format("-" * len(str(file_name)), file_name)
+            f.write(f"{title}\n{ocr_function(image_path)}\n\n")
+
+        f.truncate(f.tell() - 2)
 
 
 def main():
