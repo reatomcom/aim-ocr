c05e8d045fa3c6c5616d2e174534e0aa5f52907a
re-adrianskrievins <152814934+re-adrianskrievins@users.noreply.github.com>
2023-12-12 15:58:42 +0200

    output bounding boxes instead of plain text

--- engines.py
+++ engines.py
@@ -3,11 +3,39 @@ import pytesseract
 
 
 def run_pytesseract(image_path):
-    return pytesseract.image_to_string(image_path, lang="eng+lav")
+    output = pytesseract.image_to_data(
+        image_path, lang="eng+lav", output_type=pytesseract.Output.DICT
+    )
+
+    return [
+        (
+            text,
+            [
+                (left, top),
+                (left + width, top),
+                (left + width, top + height),
+                (left, top + height),
+            ],
+            confidence / 100,
+        )
+        for text, left, top, width, height, confidence in zip(
+            output["text"],
+            output["left"],
+            output["top"],
+            output["width"],
+            output["height"],
+            output["conf"],
+        )
+        if text.strip()
+    ]
 
 
 reader = easyocr.Reader(["en", "lv"], gpu=False, verbose=False)
 
 
 def run_easyocr(image_path):
-    return "\n".join(reader.readtext(image_path, detail=0))
+    return [
+        (text, [(int(x), int(y)) for x, y in bbox], confidence)
+        for bbox, text, confidence in reader.readtext(image_path)
+        if text.strip()
+    ]
