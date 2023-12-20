eec1698750914b393ce8939fb787ca8882b16f2f
re-adrianskrievins <152814934+re-adrianskrievins@users.noreply.github.com>
2023-12-20 22:48:07 +0200

    save all results in one file

--- main.py
+++ main.py
@@ -14,24 +14,25 @@ class DataClassEncoder(json.JSONEncoder):
         return super().default(o)
 
 
-def process_dataset(ocr_function: Callable[[str], list[engines.ScanData]]):
+def process_dataset(*ocr_funcs: Callable[[str], list[engines.ScanData]]):
     config.OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
-    output_path = config.OUTPUT_DIR.joinpath(ocr_function.__name__).with_suffix(".json")
+    output_path = config.OUTPUT_DIR.joinpath("output").with_suffix(".json")
 
     output = {}
     for image_path in sorted(config.DATASET_DIR.iterdir()):
         if not image_path.is_file():
             continue
 
-        output[image_path.name] = ocr_function(str(image_path))
+        output[image_path.name] = {
+            ocr_func.__name__: ocr_func(str(image_path)) for ocr_func in ocr_funcs
+        }
 
     with output_path.open("w", encoding="utf-8") as output_file:
         json.dump(output, output_file, ensure_ascii=False, cls=DataClassEncoder)
 
 
 def main():
-    process_dataset(engines.run_pytesseract)
-    process_dataset(engines.run_easyocr)
+    process_dataset(engines.run_pytesseract, engines.run_easyocr)
 
 
 if __name__ == "__main__":
