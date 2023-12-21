b935c32cd69cca75082e2a8110cd28d23a5edb91
re-vadimsgolevs <vadims.golevs@reatom.com>
2023-12-21 14:34:47 +0200

    hiding process_ratio function

--- main.py
+++ main.py
@@ -28,28 +28,28 @@ def process_dataset(*ocr_funcs: Callable[[str], list[engines.ScanData]]):
     return output
 
 
-def process_ratio(ocr_function):
-    config.OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
-    original_path = config.OUTPUT_DIR.joinpath("original").with_suffix(".json")
-    with open(original_path, 'r', encoding="utf-8") as original_file:
-        original_texts = json.load(original_file)
+# def process_ratio(ocr_function):
+#     config.OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
+#     original_path = config.OUTPUT_DIR.joinpath("original").with_suffix(".json")
+#     with open(original_path, 'r', encoding="utf-8") as original_file:
+#         original_texts = json.load(original_file)
 
-    output_path = config.OUTPUT_DIR.joinpath(ocr_function.__name__ + "_accuracy").with_suffix(".json")
-    output = {}
-    for image_path in config.OUTPUT_DIR.iterdir():
-        if not image_path.is_file():
-            continue
+#     output_path = config.OUTPUT_DIR.joinpath(ocr_function.__name__ + "_accuracy").with_suffix(".json")
+#     output = {}
+#     for image_path in config.OUTPUT_DIR.iterdir():
+#         if not image_path.is_file():
+#             continue
 
-        recognition = ocr_function(str(image_path))
-        if isinstance(recognition, list):
-            recognition = " ".join([item['text'] for item in recognition])
-        else:
-            recognition = recognition.replace("\n", " ")
-        ratio = fuzz.partial_token_sort_ratio(recognition, original_texts[image_path.name])
-        output[image_path.name] = [recognition, ratio]
+#         recognition = ocr_function(str(image_path))
+#         if isinstance(recognition, list):
+#             recognition = " ".join([item['text'] for item in recognition])
+#         else:
+#             recognition = recognition.replace("\n", " ")
+#         ratio = fuzz.partial_token_sort_ratio(recognition, original_texts[image_path.name])
+#         output[image_path.name] = [recognition, ratio]
     
-    with output_path.open("w", encoding="utf-8") as output_file:
-        json.dump(output, output_file, ensure_ascii=False, indent=4, sort_keys=True)
+#     with output_path.open("w", encoding="utf-8") as output_file:
+#         json.dump(output, output_file, ensure_ascii=False, indent=4, sort_keys=True)
     
 
 def main():
