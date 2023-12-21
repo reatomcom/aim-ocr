435d1ebdbd91d3e17136c21df7e40201a5fb06e1
re-adrianskrievins <152814934+re-adrianskrievins@users.noreply.github.com>
2023-12-21 16:01:09 +0200

    there may not be enough memory for all libraries in parallel

--- main.py
+++ main.py
@@ -16,14 +16,17 @@ class DataClassEncoder(json.JSONEncoder):
 
 def process_dataset(*ocr_funcs: Callable[[str], list[engines.ScanData]]):
     output = {}
-    for image_path in sorted(config.DATASET_DIR.iterdir()):
-        if not image_path.is_file():
-            continue
+    for ocr_func in ocr_funcs:
+        for image_path in sorted(config.DATASET_DIR.iterdir()):
+            if not image_path.is_file():
+                continue
 
-        output[image_path.name] = {
-            ocr_func.__name__: [sd for sd in ocr_func(str(image_path)) if sd.text]
-            for ocr_func in ocr_funcs
-        }
+            if image_path.name not in output:
+                output[image_path.name] = {}
+
+            output[image_path.name][ocr_func.__name__] = [
+                sd for sd in ocr_func(str(image_path)) if sd.text
+            ]
 
     return output
 
