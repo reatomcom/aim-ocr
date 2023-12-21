25bb2d67891974f026fc67072b3b07e2611125c0
re-adrianskrievins <152814934+re-adrianskrievins@users.noreply.github.com>
2023-12-21 16:08:58 +0200

    refactor repeated checks in process_dataset

--- main.py
+++ main.py
@@ -15,15 +15,11 @@ class DataClassEncoder(json.JSONEncoder):
 
 
 def process_dataset(*ocr_funcs: Callable[[str], list[engines.ScanData]]):
-    output = {}
+    image_paths = sorted(p for p in config.DATASET_DIR.iterdir() if p.is_file())
+    output = {p.name: {} for p in image_paths}
+
     for ocr_func in ocr_funcs:
-        for image_path in sorted(config.DATASET_DIR.iterdir()):
-            if not image_path.is_file():
-                continue
-
-            if image_path.name not in output:
-                output[image_path.name] = {}
-
+        for image_path in image_paths:
             output[image_path.name][ocr_func.__name__] = [
                 sd for sd in ocr_func(str(image_path)) if sd.text
             ]
