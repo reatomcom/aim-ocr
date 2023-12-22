5a1862b98834232ab41c3a5d5e1cec084bb98fe7
re-adrianskrievins <152814934+re-adrianskrievins@users.noreply.github.com>
2023-12-22 14:23:56 +0200

    add input file env var

--- config.py
+++ config.py
@@ -12,5 +12,5 @@ def rel2abs(path: Path):
     return path if path.is_absolute() else PROJECT_ROOT.joinpath(path)
 
 
-for path_env_var in ("DATASET_DIR", "OUTPUT_FILE"):
+for path_env_var in ("DATASET_DIR", "OUTPUT_FILE", "INPUT_FILE"):
     globals()[path_env_var] = rel2abs(Path(os.environ[path_env_var]))
